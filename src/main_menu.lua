e464126c8a7fc91445211b2fb962353c4fac3909
JohanStenius <johan.stenius@me.com>
2014-10-10 13:06:57 +0200

    Added function that allows user to scroll up and down

--- src/main_menu.lua
+++ src/main_menu.lua
@@ -1,15 +1,22 @@
-gfx = require "gfx"
+--TODO:
+--Real graphic components
+--Inputs from user, read and write
+--Create user from input
+--Buttons
+--
+
+local gfx = require "gfx"
 gfx.screen:fill({255,255,255,255})
 gfx.update()
 
--- Directory of artwork
-dir = './'
-	
--- All the numbers as .png pictures with transparent background
--- with size:
-png_logo_width = 361 
-png_logo_height = 100 
-	
+local logoSurface = gfx.new_surface(gfx.screen:get_width()/2, gfx.screen:get_height()/4)
+local sideSurface = gfx.new_surface(gfx.screen:get_width()/4, gfx.screen:get_height())
+local inputSurface = gfx.new_surface(gfx.screen:get_width()/1, gfx.screen:get_height())
+
+--Starting coordinates for current inputField
+local inputFieldX = 110
+local inputFieldY = 160
+
 png_numbers = {
 	  num1 = 'images/1.png',
 	  num2 = 'images/2.png',
@@ -22,41 +29,95 @@ png_numbers = {
 	  num9 = 'images/9.png',
 	  num0 = 'images/0.png',
 }
+
+--Calls methods that builds GUI
 function buildGUI()
--- Logo
+
 displayLogo()
---Buttons
---displayButtons()
---Text
---printText(Text)
+displaySideSurface()
+drawInputSurface()
 end
 
+--Creates new surface and displays logo
 function displayLogo()
-
+	logoSurface:clear()
+	logoSurface:fill({255,255,255})
+	gfx.screen:copyfrom(logoSurface,nil,{x=0, y=0})
+	png_logo_width = 361 
 	local logo = gfx.loadpng("images/logo.png")
     gfx.screen:copyfrom(logo, nil, {x=gfx.screen:get_width()/2-(png_logo_width/2), y=100})
 	gfx.update()
-
+end
+--Creates new surface and displays items on the left side
+function displaySideSurface()
+	sideSurface:clear()
+	sideSurface:fill({255,255,255})
+	gfx.screen:copyfrom(sideSurface,nil,{x=0, y=200})
+	gfx.update()
+	--name
+	local name = gfx.loadpng("images/1.png")
+	printPicture(name,20,160)
+	--adress
+	local adress = gfx.loadpng("images/2.png")
+	printPicture(adress,20,230)
+	--postal
+	local postal = gfx.loadpng("images/3.png")
+	printPicture(postal,20,300)
+	--phone
+	local phone = gfx.loadpng("images/4.png")
+	printPicture(phone,20,370)
+	--email
+	gfx.update()
 end
 
-function displayButtons()
---TODO
---Code to display buttons
---Use table with images of buttons
+--Creates inputsurface and displays "highlighted" input
+function drawInputSurface()
+	inputSurface:clear()
+	inputSurface:fill({255,255,255})
+	gfx.screen:copyfrom(inputSurface,nil,{x=120, y=200})
+	local input = gfx.loadpng("images/0.png")
+	printPicture(input,inputFieldX,inputFieldY)
+	gfx.update()
+end
+
+--Moves the current inputField
+function moveInputFields(key)
+	if(key == 'red') then
+		if(inputFieldY > 160) then
+			inputFieldY = inputFieldY - 70
+			drawInputSurface()
+		end
+	end
+	if(key == 'green') then
+		if(inputFieldY < 370) then
+			inputFieldY = inputFieldY + 70
+			drawInputSurface()
+		end
+	end
+end
+--Method that prints picture to screen. Takes picture and x,y coordinates as argument.
+function printPicture(pic,xx,yy)
+ 	gfx.screen:copyfrom(pic, nil, {x=xx,y=yy})
 
 end
 
 function onKey(key,state)
-  if(key == 'red') then
-  elseif(key == 'green') then
-  elseif(key == 'yellow') then
-  elseif(key == 'blue') then
-  end
+  	if(key == 'red') then
+  		--Up
+  		moveInputFields(key)
+  	elseif(key == 'green') then
+  		--Down
+  		moveInputFields(key)
+  	elseif(key == 'yellow') then
+
+  	elseif(key == 'blue') then
+  	end
 	gfx.update()
 end
 
+--Main method
 function main()
-buildGUI()
+	buildGUI()
 
 end
 main()

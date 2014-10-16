7de9a3dfab929047ab88b1a0694fb5636d5988bc
JohanStenius <johan.stenius@me.com>
2014-10-16 11:04:11 +0200

    Changed onKey buttons to Up,Down,Left,Right

--- src/choose_Pizzeria.lua
+++ src/choose_Pizzeria.lua
@@ -7,13 +7,12 @@ local gfx = require "gfx"
 local inputFieldY = 250
 local inputFieldX = gfx.screen:get_width()/8
 
-gfx.screen:fill({0,0,0})
+gfx.screen:fill({241,248,233})
 gfx.update()
 
-local logoSurface = gfx.new_surface(gfx.screen:get_width()/2, gfx.screen:get_height()/4)
-local sideSurface = gfx.new_surface(gfx.screen:get_width()/4, gfx.screen:get_height())
+local logoSurface = gfx.new_surface(gfx.screen:get_width(), gfx.screen:get_height()/5)
 local pizzeriaSurface = gfx.new_surface(gfx.screen:get_width(), gfx.screen:get_height()/2)
-local highlightSurface = gfx.new_surface(gfx.screen:get_width(), 80)
+--local highlightSurface = gfx.new_surface(gfx.screen:get_width(), 80)
 
 
 --Calls methods that builds GUI
@@ -22,33 +21,23 @@ function buildGUI()
 	displayLogo()
 	--displayInputSurface()
 	displayPizzerias()
-	displayHighlightSurface()
 end
 
 --Creates new surface and displays logo
 function displayLogo()
 	logoSurface:clear()
-	logoSurface:fill({0,0,0})
+	logoSurface:fill({139,195,74})
 	gfx.screen:copyfrom(logoSurface,nil,{x=0, y=0})
 	png_logo_width = 250
 	text.print(gfx.screen,arial,"InstantPizza",gfx.screen:get_width()/2-220/2,50,220,300)
-	text.print(gfx.screen,arial,"Pizzerias near you",gfx.screen:get_width()/2-320/2,150,320,300)
+	text.print(gfx.screen,arial,"Pizzerias near you",gfx.screen:get_width()/2-320/2,160,320,300)
 	gfx.update()
 end
---Creates inputsurface and displays "highlighted" input
-function displayHighlightSurface()
-	--TODO:
-	--Actual inputfields and higlight. Variable highlight is the actual highlight
-	--that moves when user presses up and down.
-	--Try transparent on box
-
-end
-
 function displayPizzerias()
 	--TODO
 	--Get pizzerias
 	pizzeriaSurface:clear()
-	pizzeriaSurface:fill({0,0,0})
+	pizzeriaSurface:fill({241,248,233})
 	gfx.screen:copyfrom(pizzeriaSurface,nil,{x=0, y=230})
 	text.print(gfx.screen,arial,"Pizzeria 1",gfx.screen:get_width()/8,300,220,300)
 	text.print(gfx.screen,arial,"Pizzeria 2",gfx.screen:get_width()/8+200,300,220,300)
@@ -67,32 +56,40 @@ function moveHighlightedInputField(key)
 	--Starting coordinates for current inputField
 	 inputFieldStart = 150
 	 inputFieldEnd = 550
+	 print(inputFieldX)
 	--Up
-	if(key == 'red')then
+	if(key == 'Up')then
 		if(inputFieldY == 450) then
 			inputFieldY = 250
 			displayPizzerias()
 		end
 	end
 	--Down
-	if(key == 'green')then
+	if(key == 'Down')then
 		if(inputFieldY == 250)then
 			inputFieldY = 450
 			displayPizzerias()
 		end
 	end
 	--Left
-	if(key == 'yellow')then
+	if(key == 'Left')then
 		if(inputFieldX > gfx.screen:get_width()/8) then
 			inputFieldX = inputFieldX - 200
+			displayPizzerias()
+		elseif(inputFieldX == gfx.screen:get_width()/8)then
+			inputFieldX = gfx.screen:get_width()/8+600
 			displayPizzerias()
 		end
 	end
 	--Down
-	if(key == 'blue') then
+	if(key == 'Right') then
 		if(inputFieldX < gfx.screen:get_width()/8+600) then
 			inputFieldX = inputFieldX + 200 
 			displayPizzerias()
+		elseif(inputFieldX == gfx.screen:get_width()/8+600)then
+			inputFieldX = gfx.screen:get_width()/8
+			displayPizzerias()
+
 		end
 	end
 end
@@ -106,17 +103,18 @@ end
 function onKey(key,state)
 	--TODO
 
-  	if(key == 'red') then
+  	if(key == 'Up') then
   		--Up
+  		print("Hej")
   		moveHighlightedInputField(key)
-  	elseif(key == 'green') then
+  	elseif(key == 'Down') then
   		--Down
   	  	moveHighlightedInputField(key)
   		--Left
-  	elseif(key == 'yellow') then
+  	elseif(key == 'Left') then
   		moveHighlightedInputField(key)
   		--Right
-  	elseif(key == 'blue') then
+  	elseif(key == 'Right') then
   		moveHighlightedInputField(key)
 
   	end

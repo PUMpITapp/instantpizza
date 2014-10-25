c6ef080e631776f087b338a8a58f4ab99597b1d6
Huy Tran <huytr402@student.liu.se>
2014-10-25 17:23:09 +0200

    keyboard scalable to screen mapping of keys changed from float->int

--- src/keyboard.lua
+++ src/keyboard.lua
@@ -1,22 +1,29 @@
 local gfx = require "gfx"
 local text = require "write_text"
 local keyboardPNG = gfx.loadpng("keyboard2.png")
-gfx.screen:fill({0,0,0,0})
-gfx.update()
 
-
+--what do these actually do?!
 local keyboardSurface = gfx.new_surface(gfx.screen:get_width(), gfx.screen:get_height())
 local highlightSurface = gfx.new_surface(gfx.screen:get_width(), gfx.screen:get_height())
-local inputSurface = gfx.new_surface(gfx.screen:get_width()/2, gfx.screen:get_height()/2)
+local inputSurface = gfx.new_surface(gfx.screen:get_width()/10, gfx.screen:get_height()/10)
+--
+
+local screenWidth = gfx.screen:get_width()
+local screenHeight = gfx.screen:get_height()
 
 local inputSurfaceX = 0	-- used for writing letters after each other in inputdisplay
 local inputSurfaceY = 0	-- used for writing letters after each other in inputdisplay
-local xMargin = 37 -- margin in x for keyboard keys
-local yMargin = 38 -- margin in y for keyboard keys
+local xMargin = screenWidth * 8/10/10 -- margin in x for keyboard keys. 10 keys each row
+local yMargin = screenHeight * 6/10/4 -- margin in y for keyboard keys. 4 keys each column
 local keyboardWidth = 10 * xMargin	-- puts boundaries for highlight
 local keyboardHeight = 4 * yMargin	-- puts boundaries for highlight
 local lastInputX = xMargin	-- last input of x
 local lastInputY = yMargin	-- last input of y
+
+gfx.screen:fill({0,0,0,0}) --colours the screen black
+gfx.screen:fill({r=255, g=255, b=255, a=0}, {x=screenWidth/10, y=screenHeight/10, w=screenWidth * 8/10, h=screenHeight/10}) --colours the input field
+gfx.update()
+
 local PosKey = {}
 
 -- creates an object with position of the key
@@ -24,10 +31,10 @@ function PosKey:new(key, posX, posY)
 	key = {	letter = key,
 			row = posX,		-- what row the key is in
 			column = posY,	-- what column the key is in
-			x = xMargin * posX,	-- x starting position
-			y = yMargin * posY,	-- y starting position
-			w = 200,		-- width
-			h = 300			-- height
+			x = math.floor(xMargin * posX),	-- x starting position. making it an int for precision
+			y = math.floor(yMargin * posY),	-- y starting position. making it an int for precision
+			w = 200,		-- width not used
+			h = 300			-- height not used
 			}
 	self.__index = self
 	return setmetatable(key ,self)
@@ -38,21 +45,7 @@ local keyboard = {
 	-- the whole keyboard is not implemented yet
 
 	--first row
-		-- GraveAccent = PosKey:new("`",1,1),
-		-- one = PosKey:new("1",2,1),
-		-- two = PosKey:new("2",3,1),
-		-- three = PosKey:new("3",4,1),
-		-- four = PosKey:new("4",5,1),
-		-- five = PosKey:new("5",6,1),
-		-- six = PosKey:new("6",7,1),
-		-- seven = PosKey:new("7",8,1),
-		-- eight = PosKey:new("8",9,1),
-		-- nine = PosKey:new("9",10,1),
-		-- zero = PosKey:new("0",11,1),
-		-- minus = PosKey:new("-",12,1),
-		-- equal = PosKey:new("=",13,1),
-		-- backspace1 = PosKey:new("BACKSPACE", 14,1),
-		-- backspace2 = PosKey:new("BACKSPACE", 15,1),
+
 		one = PosKey:new("1",1,1),
 		two = PosKey:new("2",2,1),
 		three = PosKey:new("3",3,1),
@@ -63,22 +56,9 @@ local keyboard = {
 		eight = PosKey:new("8",8,1),
 		nine = PosKey:new("9",9,1),
 		zero = PosKey:new("0",10,1),
-
 	
 	-- second row
-		-- Tab = PosKey:new("TAB",1,2),
-		-- Q = PosKey:new("Q",2,2),
-		-- W = PosKey:new("W",3,2),
-		-- E = PosKey:new("E",4,2),
-		-- R = PosKey:new("R",5,2),
-		-- T = PosKey:new("T",6,2),
-		-- Y = PosKey:new("Y",7,2),
-		-- U = PosKey:new("U",8,2),
-		-- I = PosKey:new("I",9,2),
-		-- O = PosKey:new("O",10,2),
-		-- P = PosKey:new("P",11,2),
-		-- leftbracket = PosKey:new("LEFTBRACKET",12 ,2),
-		-- rightbracket= PosKey:new("RIGHTBRACKET",13 ,2),
+
 		Q = PosKey:new("Q",1,2),
 		W = PosKey:new("W",2,2),
 		E = PosKey:new("E",3,2),
@@ -90,16 +70,7 @@ local keyboard = {
 		O = PosKey:new("O",9,2),
 		P = PosKey:new("P",10,2),
 	-- third row
-		-- Caps = PosKey:new("CAPS",1,3),
-		-- A = PosKey:new("A",2,3),
-		-- S = PosKey:new("S",3,3),
-		-- D = PosKey:new("D",4,3),
-		-- F = PosKey:new("F",5,3),
-		-- G = PosKey:new("G",6,3),
-		-- H = PosKey:new("H",7,3),
-		-- J = PosKey:new("J",8,3),
-		-- K = PosKey:new("K",9,3),
-		-- L = PosKey:new("L",10,3),
+
 		A = PosKey:new("A",1,3),
 		S = PosKey:new("S",2,3),
 		D = PosKey:new("D",3,3),
@@ -112,25 +83,10 @@ local keyboard = {
 		Shift = PosKey:new("Shift",10,3),
 	-- fourth row
 		
-		-- Lshift1 = PosKey:new("Lshift",1,4),
-		-- Lshift2 = PosKey:new("Lshift",2,4),
-		-- Z = PosKey:new("Z",3,4),
-		-- X = PosKey:new("X",4,4),
-		-- C = PosKey:new("C",5,4),
-		-- V = PosKey:new("V",6,4),
-		-- B = PosKey:new("B",7,4),
-		-- N = PosKey:new("N",8,4),
-		-- M = PosKey:new("M",9,4),
-		-- Colon = PosKey:new(",",10,4),
-		-- Period = PosKey:new(".",11,4),
-		-- Question = PosKey:new("?",12,4),
-		-- Rshift1 = PosKey:new("Rshift", 13, 4),
-		-- Rshift2 = PosKey:new("Rshift", 14, 4),
-		-- Rshift3 = PosKey:new("Rshift", 15, 4)
 		Z = PosKey:new("Z",1,4),
 		X = PosKey:new("X",2,4),
 		C = PosKey:new("C",3,4),
-		V = PosKey:new("V",4,4),
+		V = PosKey:new("V",4,4), 
 		B = PosKey:new("B",5,4),
 		N = PosKey:new("N",6,4),
 		M = PosKey:new("M",7,4),
@@ -149,12 +105,11 @@ end
 
 --display keyboard
 function displayKeyboardSurface()
+	local width = gfx.screen:get_width()
+	local height = gfx.screen:get_height()
 	keyboardSurface:clear()
 	keyboardSurface:fill({0,0,0})
-	gfx.screen:copyfrom(keyboardPNG, nil, {x=xMargin, y=yMargin})
-	-- for key, value in pairs(keyboard)do
-	-- 	text.print(gfx.screen, arial, value.letter, value.x, value.y, value.w, value.h)
-	-- end
+	gfx.screen:copyfrom(keyboardPNG, nil, {x=screenWidth/10, y=screenHeight * 3/10, w=screenWidth * 8/10, h=screenHeight * 6/10})
 	gfx.update()
 end
 
@@ -162,12 +117,14 @@ end
 -- TODO
 -- needs to change position of copyfrom. (0,0) now writes over keyboard 
 function displayHighlightSurface()
+	local width = gfx.screen:get_width()
+	local height = gfx.screen:get_height()
 
 	highlightSurface:clear()
 	highlightSurface:fill({255,0,0,0})
 	--gfx.screen:copyfrom(keyboardSurface, nil, {x=600, y=600})
 	displayKeyboardSurface()
-	gfx.screen:fill({r=255, g=0, b=0, a=0}, {x=lastInputX, y=lastInputY, w=35, h=35})
+	gfx.screen:fill({r=255, g=0, b=0, a=0}, {x=screenWidth * 1/10 + lastInputX - xMargin, y=screenHeight * 3/10 + lastInputY - yMargin, w=xMargin, h=yMargin})
 	gfx.update()
 end
 
@@ -234,21 +191,23 @@ end
 
 -- temporarely displays the letter on screen.
 function displayInputSurface(letter)
-	local startX = 200
-	local startY = 200
-	startX = startX + inputSurfaceX
-	startY = startY + inputSurfaceY
+	local startX = screenWidth/10
+	local startY = screenHeight * 1/10
+
+	text.print(gfx.screen, arial, letter, screenWidth/10 + inputSurfaceX, screenHeight * 1/10 + inputSurfaceY, screenWidth * 8/10, screenHeight/10)
+
 	inputSurfaceX = inputSurfaceX + 30
 
-	if(startX>gfx.screen:get_width()) then
+	if(inputSurfaceX>(screenWidth * 8/10)) then
 	inputSurfaceY = inputSurfaceY + 30
 	inputSurfaceX = 0
-	startY = startY + inputSurfaceY
 	end
 
-	inputSurface:clear()
-	inputSurface:fill({0,0,0,0})
-	text.print(gfx.screen, arial, letter, startX, startY, 200, 300)
+	-- inputSurface:clear()
+	-- inputSurface:fill({255,255,255,0})
+
+
+	gfx.update()
 end
 
 -- calls functions on keys
@@ -275,8 +234,11 @@ end
 -- gets the char that is highlighted
 function getKeyboardChar(posX, posY)
 	local letter = nil
+	local cursorPosX = math.floor(posX)	--int for precision
+	local cursorPosY = math.floor(posY) --int for precision
+
 	for key, value in pairs(keyboard) do
-		if(posX == value.x) and (posY==value.y)then
+		if(cursorPosX == value.x) and (cursorPosY==value.y)then
 
 			print(value.letter)
 			return value.letter

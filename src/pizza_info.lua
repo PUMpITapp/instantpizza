30183acfc569da34ef0d35cabd432bf8c75316e0
Huy Tran <huytr402@student.liu.se>
2014-11-06 18:54:34 +0100

    add correct background to pizzainfo add highlighter to pizzainfo

--- src/pizza_info.lua
+++ src/pizza_info.lua
@@ -1,30 +1,30 @@
---TODO:
---Another background and text font/color
---Real graphic components
---Inputs from user, read and write
---Create user from input
---Buttons
---Transparency not working
-
 local text = require "write_text"
 local gfx = require "gfx"
--- inserted these for showing to customer. remove if needed //Huy
-
-local background = gfx.loadpng("images/ChoosePizzeria/chosepizzeria.png")
 
 local xUnit = gfx.screen:get_width()/16
 local yUnit = gfx.screen:get_height()/9
 
---Start of inputFields. Needed for 
 local pizzaFieldY = yUnit * 2.5
 local pizzaFieldX = xUnit * 3
 
-gfx.screen:fill({241,248,233})
-gfx.update()
+local pizzaCellX = 10/4 * xUnit
+local pizzaCellY = 4/2 * yUnit
 
+local highlightPosX = 1
+local highlightPosY = 1
+
+local pizzaLimitX = 4
+local pizzaLimitY = 2
+
+local highligtherPNG = gfx.loadpng("images/pizzaInfo/pizzeriatile.png")
+local backgroundPNG = gfx.loadpng("images/pizzaInfo/choosePizzas.png")
 
 local pizzaSurface = gfx.new_surface(10 * xUnit, 4 * yUnit)
 local backgroundSurface = gfx.new_surface(gfx.screen:get_width(), gfx.screen:get_height())
+local highlightSurface = gfx.new_surface(10 * xUnit, 4 * yUnit)
+
+local lastStateForm = ...
+
 
 local pizzaMenu = {
 	p1 = {name = "Kebab", price = 75},
@@ -41,12 +41,13 @@ local pizzaMenu = {
 function updateScreen()
 displayBackground()
 displayPizzas()
+displayHighlightSurface()
 gfx.update()
 end
 
 function displayBackground()
 	backgroundSurface:clear()
-	backgroundSurface:copyfrom(background)
+	backgroundSurface:copyfrom(backgroundPNG)
 
 	gfx.screen:copyfrom(backgroundSurface)
 end
@@ -55,12 +56,13 @@ end
 function displayPizzas()
 	local pizzaPosX = pizzaFieldX
 	local pizzaPosY = pizzaFieldY
-	local times = 0;
+	local times = 0
+
 	pizzaSurface:clear()
 	pizzaSurface:fill({241,248,233})
 	for key, value in pairs(pizzaMenu) do
 		text.print(gfx.screen, arial, value.name, pizzaPosX, pizzaPosY, 2.5 * xUnit, 2 * yUnit)
-		text.print(gfx.screen, arial, tostring(value.price), pizzaPosX, pizzaPosY + 0.5 * yUnit, 2.5 * xUnit, 2 * yUnit)
+		text.print(gfx.screen, arial, tostring(value.price) .. "kr", pizzaPosX, pizzaPosY + 0.5 * yUnit, pizzaCellX, pizzaCellY)
 		pizzaPosX = pizzaPosX + 2.5 * xUnit
 		times = times + 1
 		if(times > 3)then
@@ -73,57 +75,43 @@ end
 
 --Creates inputsurface and displays "highlighted" input
 function displayHighlightSurface()
-
+	
+	highlightSurface:clear()
+	highlightSurface:copyfrom(highligtherPNG)
+	gfx.screen:copyfrom(highlightSurface, nil , {x = pizzaFieldX + (highlightPosX -1)*pizzaCellX, y = pizzaFieldY +(highlightPosY-1) * pizzaCellY, w =pizzaCellX, h =pizzaCellY})
 end
 
 
 
 --Moves the current inputField
-function moveHighlightedInputField(key)
-	--Starting coordinates for current inputField
-	 inputFieldStart = 150
-	 inputFieldEnd = 550
+function moveHighlight(key)
+
 	--Up
 	if(key == 'Up')then
-		if(inputFieldY == 450) then
-			inputFieldY = 250
-			displayPizzas()
+		highlightPosY = highlightPosY - 1
+		if(highlightPosY < 1) then
+			highlightPosY = highlightPosY +1
 		end
-	end
+
 	--Down
-	if(key == 'Down')then
-		if(inputFieldY == 250)then
-			inputFieldY = 450
-			displayPizzas()
+	elseif(key == 'Down')then
+		highlightPosY = highlightPosY + 1
+		if(highlightPosY > pizzaLimitY) then
+			highlightPosY = highlightPosY -1
 		end
-	end
 	--Left
-	if(key == 'Left')then
-		if(inputFieldX > gfx.screen:get_width()/8) then
-			inputFieldX = inputFieldX - 200
-			displayPizzas()
-		elseif(inputFieldX == gfx.screen:get_width()/8)then
-			inputFieldX = gfx.screen:get_width()/8+600
-			displayPizzas()
+	elseif(key == 'Left')then
+		highlightPosX = highlightPosX - 1
+		if(highlightPosX < 1) then
+			highlightPosX = highlightPosX +1
+		end
+	--Right
+	elseif(key == 'Right') then
+		highlightPosX = highlightPosX + 1
+		if(highlightPosX > pizzaLimitX) then
+			highlightPosX = highlightPosX -1
 		end
 	end
-	--Down
-	if(key == 'Right') then
-		if(inputFieldX < gfx.screen:get_width()/8+600) then
-			inputFieldX = inputFieldX + 200 
-			displayPizzas()
-		elseif(inputFieldX == gfx.screen:get_width()/8+600)then
-			inputFieldX = gfx.screen:get_width()/8
-			displayPizzas()
-
-		end
-	end
-end
-
---Method that prints picture to screen. Takes picture and x,y coordinates as argument.
-function printPicture(pic,xx,yy)
- 	gfx.screen:copyfrom(pic, nil, {x=xx,y=yy})
-
 end
 
 function onKey(key,state)
@@ -131,18 +119,25 @@ function onKey(key,state)
 	if(state == 'up') then
 	  	if(key == 'Up') then
 	  		--Up
-	  		moveHighlightedInputField(key)
+	  		moveHighlight(key)
+	  		updateScreen()
+	  		
 	  	elseif(key == 'Down') then
 	  		--Down
-	  	  	moveHighlightedInputField(key)
+	  		moveHighlight(key)
+	  		updateScreen()
 	  		--Left
 	  	elseif(key == 'Left') then
-	  		moveHighlightedInputField(key)
+	  		moveHighlight(key)
+	  		updateScreen()
 	  		--Right
 	  	elseif(key == 'Right') then
-	  		moveHighlightedInputField(key)
+	  		moveHighlight(key)
+			updateScreen()
 	  	elseif(key == 'red') then
-	  	  	dofile("choose_Pizzeria.lua")
+	  		assert(loadfile("choose_Pizzeria.lua"))(nil)
+	  	elseif(key == 'blue') then
+
 	  	end
 	end
 	gfx.update()
@@ -150,7 +145,7 @@ end
 
 --Main method
 function main()
-	updateScreen()
+	updateScreen(q)
 end
 main()
 

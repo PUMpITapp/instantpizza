32df9fa0e47f61ea941dc1e959463d1135e7d8e7
Pierre Leca <piele739@student.liu.se>
2014-11-08 01:06:51 +0100

    sample unit test

---
+++ src/spec/tests_spec.lua
@@ -0,0 +1,11 @@
+
+describe("Tests:", function()
+  it("test some functionalities", function()
+	local weLikeUnitTest = true
+	local weWillHaveSomeTestsInMasterBranch = true
+  	
+	assert.is_true(weLikeUnitTest)
+	assert.is_true(weWillHaveSomeTestsInMasterBranch)
+  end)
+
+end)

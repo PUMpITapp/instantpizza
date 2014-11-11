e45e9792e1ab09144239bc321a4765c653ab6bba
JohanStenius <johan.stenius@me.com>
2014-11-11 14:11:22 +0100

    Created IOHandler, reads and writes objects to file. Added functionality to display pizzerias from file in RegistrationStep2.

---
+++ src/IOHandler.lua
@@ -0,0 +1,109 @@
+local ioHandler = {}
+--Create new user
+User = {}
+function User:new(name,address,zipCode,city,phone,email,pizzeria,pizzaOne,pizzaTwo,pizzaThree,pizzaFour)
+  newObj ={
+  name = name,
+  address = address,
+  zipCode = zipCode,
+  city = city,
+  phone = phone,
+  email = email,
+  pizzeria = pizzeria,
+  pizzaOne = pizzaOne,
+  pizzaTwo = pizzaTwo,
+  pizzaThree = pizzaThree,
+  pizzaFour = pizzaFour
+  }
+  self.__index = self
+  return setmetatable(newObj, self)
+end
+--Create new pizzeria. Each pizzeria object has one table of pizzas. 
+Pizzeria = {}
+function Pizzeria:new(id,name,imgPath,rating,pizzas)
+  newObj = {
+  id = id,
+  name = name,
+  imgPath = imgPath,
+  rating = rating,
+  pizzas = pizzas
+  }
+  self.__index = self
+  return setmetatable(newObj, self)
+end
+--Create pizza
+Pizza = {}
+function Pizza:new(id,name,price)
+  newObj = {
+  id = id,
+  name = name,
+  price = price
+  }
+  self.__index = self
+  return setmetatable(newObj, self)
+end
+--Function to split string. Delimiter is where to split the string. Our files uses ", "
+function string:split(delimiter)
+  result = {}
+  local start = 1
+  local splitStart, splitEnd = string.find( self, delimiter, splitStart )
+  while splitStart do
+    table.insert( result, string.sub( self, start, splitStart-1 ))
+    start = splitEnd + 1
+    splitStart, splitEnd = string.find( self, delimiter, start )
+  end
+  table.insert( result, string.sub( self, start ) )
+  return result
+end
+--Function to read users. Users are stored in a table containing user "objects"
+function ioHandler.readUsers()
+  local usersTable = {}
+  i = 1
+  for line in io.open("TextFiles/users.txt"):lines()do
+    local userTable = line:split(", ")
+    usersTable[i] = User:new(userTable[1],userTable[2],userTable[3],userTable[4],userTable[5],userTable[6],userTable[7],userTable[8],userTable[9],userTable[10],userTable[11])
+    i = i+1
+  end
+  return usersTable
+end
+-- Function to read pizzerias. Are stored in a table, every pizzeria has a table of pizzas. 
+function ioHandler.readPizzerias()
+  local pizzeriaTable = {}
+  local pizzaTable = {}
+  i = 1
+  for line in io.open("TextFiles/pizzerias.txt"):lines()do
+    local splitTable = line:split(", ")
+    pizzaTable = ioHandler.readPizzas(splitTable[1])
+    pizzeriaTable[i] = Pizzeria:new(splitTable[1], splitTable[2], splitTable[3], splitTable[4])
+    pizzeriaTable[i].pizzas = pizzaTable
+    i = i+1
+  end
+  return pizzeriaTable
+end
+--Function to read pizzas. Parameter id matches pizza to pizzeria. Return table of pizzas. 
+function ioHandler.readPizzas(id)
+  local pizzas = {}
+  j = 1
+  for line in io.open("TextFiles/pizzas.txt"):lines()do
+    local splitTable = line:split(", ")
+    --If pizzeria id matches id in pizza file
+    if(splitTable[1] == id)then
+      pizzas[j] = Pizza:new(splitTable[1],splitTable[2],splitTable[3])
+      j=j+1
+    end
+  end
+  return pizzas
+end
+
+--Function to save user to file. Appends string from userForm and saves it to file, file info i appended so nothing is overwritten. 
+function ioHandler.saveUser(userForm)
+  --Open file, append mode
+  file = io.open("TextFiles/users.txt", "a")
+  io.output(file)
+  --Append string. Not the best solution at the moment, hard to iterate through table because the order is not sorted
+  userString = userForm.name .. ", " .. userForm.address .. ", " .. userForm.zipCode .. ", " .. userForm.city .. ", " .. userForm.phone .. ", " .. userForm.email .. ", " .. userForm.pizzeria .. ", " .. userForm.pizzaOne .. ", " .. userForm.pizzaTwo .. ", " .. userForm.pizzaThree .. ", " .. userForm.pizzaFour
+  io.write("\n")
+  io.write(userString)
+  io.close()
+end
+return ioHandler
--- src/RegistrationStep2.lua
+++ src/RegistrationStep2.lua
@@ -33,6 +33,7 @@ function chooseText(underGoingTest)
   end
   return tempText
 end
+local io = require "IOHandler"
 local text = chooseText(checkTestMode())
 local gfx =  chooseGfx(checkTestMode())
 
@@ -40,32 +41,27 @@ local gfx =  chooseGfx(checkTestMode())
 local xUnit = gfx.screen:get_width()/16
 local yUnit = gfx.screen:get_height()/9
 
+
  --Start of inputFields. Needed for 
 local inputMovement = yUnit*1.2
 local inputFieldX = gfx.screen:get_width()/8
 local inputFieldY = yUnit*2.5
 local inputFieldStart = yUnit*2.5
 local inputFieldEnd = inputFieldStart+inputMovement*3
-local choosenPizzeria = true
+local choosenPizzeria = false
 local pizzeriaToAdd = ""
 
 local background = gfx.loadpng("Images/PizzeriaPics/background.png")
 local inputField = gfx.loadpng("Images/PizzeriaPics/inputfield.png")
 local highlight = gfx.loadpng("Images/PizzeriaPics/highlighter.png")
-local pizza1 = gfx.loadpng("Images/PizzeriaPics/Pizzerias/pizza1.png")
-local pizza2 = gfx.loadpng("Images/PizzeriaPics/Pizzerias/pizza2.png")
-local pizza3 = gfx.loadpng("Images/PizzeriaPics/Pizzerias/pizza3.png")
-local pizza4 = gfx.loadpng("Images/PizzeriaPics/Pizzerias/pizza4.png")
 
 local lastForm = ...
 local newForm = {}
 
-local pizzerias = {
-	picFilePath = "",
-	pizzeriaName = "",
-	description = ""
-	--Mer info?
-}
+--Pizzeria tables
+local pizzerias = {}
+local pizzeria = {}
+local highlightFieldPos = 1
 
 function checkForm()
 	if type(lastForm) == "string" then
@@ -87,65 +83,50 @@ function checkForm()
 		print(k,v)
 	end
 end
-
+--Reads pizzerias from file. Returns a table containing pizzeria objects. 
 function readPizzeriaFromFile()
-
-
-
+	pizzerias = io.readPizzerias()
 end
---Calls methods that builds GUI
+--Builds GUI
 function buildGUI()
 	gfx.screen:fill({241,248,233})
-
 	gfx.screen:copyfrom(background, nil, {x=0 , y=0, w=gfx.screen:get_width(), h=gfx.screen:get_height()})
-	
-	for i = inputFieldStart, inputFieldEnd,inputMovement do
-		gfx.screen:copyfrom(inputField,nil,{x=xUnit*3, y=i, h=xUnit, w=yUnit*7})
-	end
 	displayHighlightSurface()
 	displayPizzerias()
-	gfx.update()
+	displayChoosenPizzeria()
 end
-
-function displayPizzerias()
-	--Get pizzerias from file
-	--Display pictures
-	
-	gfx.screen:copyfrom(pizza1,nil,{x=xUnit*3, y=inputFieldStart, h=xUnit, w=yUnit*2})
-	gfx.screen:copyfrom(pizza2,nil,{x=xUnit*3, y=(inputFieldStart+inputMovement), h=xUnit, w=yUnit*2})
-	gfx.screen:copyfrom(pizza3,nil,{x=xUnit*3, y=inputFieldStart+(inputMovement*2), h=xUnit, w=yUnit*2})
-	gfx.screen:copyfrom(pizza4,nil,{x=xUnit*3, y=inputFieldStart+(inputMovement*3), h=xUnit, w=yUnit*2})
-	-- text.print(gfx.screen, arial,"Pizzeria", xUnit*13, yUnit*1, 5 * xUnit, 5 * yUnit)
-
-end
-
-function addPizzeria()
-	--Highligt pizzeria and add to form
-	-- local pizzeriaToAdd = nil
-	local pizza1YCoordinate = inputFieldStart
-	local pizza2YCoordinate = (inputFieldStart+inputMovement)
-	local pizza3YCoordinate = inputFieldStart+(inputMovement*2)
-	local pizza4YCoordinate = inputFieldStart+(inputMovement*3)
-
-	if(inputFieldY == pizza1YCoordinate)then
-		text.print(gfx.screen, arial,"Pizzeria 1", xUnit*13, yUnit*1.5, 5 * xUnit, 5 * yUnit)
-		pizzeriaToAdd = "pizzeria1"
-	elseif(inputFieldY == pizza2YCoordinate)then
-		text.print(gfx.screen, arial,"Pizzeria 2", xUnit*13, yUnit*1.5, 5 * xUnit, 5 * yUnit)
-		pizzeriaToAdd = "pizzeria2"
-	elseif(inputFieldY == pizza3YCoordinate)then
-		text.print(gfx.screen, arial,"Pizzeria 3", xUnit*13, yUnit*1.5, 5 * xUnit, 5 * yUnit)
-		pizzeriaToAdd = "pizzeria3"
-	elseif(inputFieldY == pizza4YCoordinate)then
-		text.print(gfx.screen, arial,"Pizzeria 4", xUnit*13, yUnit*1.5, 5 * xUnit, 5 * yUnit)
-		pizzeriaToAdd = "pizzeria4"
-
+--Displays chosen pizzeria in cart
+function displayChoosenPizzeria()
+	if (choosenPizzeria)then
+		text.print(gfx.screen, arial,pizzeria.name, xUnit*13, yUnit*1.5, 5 * xUnit, 5 * yUnit)
 	end
-	gfx.update()
 end
-
+--Display pizzerias. TODO: Add functionality to display more than four pizzerias. And display only in zip code area
+function displayPizzerias()
+	yCoord = inputFieldStart
+	counter = 1
+	for index,value in ipairs(pizzerias) do
+		pngPath = pizzerias[index].imgPath
+		pizzeriaImg = gfx.loadpng("Images/PizzeriaPics/Pizzerias/"..tostring(pngPath))
+		gfx.screen:copyfrom(inputField,nil,{x=xUnit*3, y=yCoord, h=xUnit, w=yUnit*7})
+		gfx.screen:copyfrom(pizzeriaImg,nil,{x=xUnit*3, y=yCoord, h=xUnit, w=yUnit*2})
+		yCoord = yCoord+inputMovement
+		if(counter == 4)then
+			break
+		end
+		counter = counter+1
+	end
+end
+--Find the selected pizzeria and send i to addToForm()
+function addPizzeria()
+	pizzeria = pizzerias[highlightFieldPos]
+	addToForm(pizzeria)
+	choosenPizzeria = true
+end
+--Adds pizzeria to form
 function addToForm(pizzeria)
 	newForm["pizzeria"] = pizzeria
+	print(pizzeria.name)
 end
 
 function displayHighlightSurface()
@@ -158,18 +139,20 @@ function moveHighlightedInputField(key)
 	if(key == 'Up') then
 		if(inputFieldY > inputFieldStart) then
 			inputFieldY = inputFieldY - inputMovement
+			highlightFieldPos = highlightFieldPos -1
 		end
 	end
 	--Down
 	if(key == 'Down') then
 		if(inputFieldY < inputFieldEnd)then
 			inputFieldY = inputFieldY + inputMovement
+			highlightFieldPos = highlightFieldPos + 1
 		
 		elseif(inputFieldY == inputFieldEnd) then
 			inputFieldY = inputFieldStart
+			highlightFieldPos = 1
 		end
 	end
-	buildGUI()
 end
 
 --Method that prints picture to screen. Takes picture and x,y coordinates as argument.
@@ -177,21 +160,25 @@ function printPicture(pic,xx,yy)
  	gfx.screen:copyfrom(pic, nil, {x=xx,y=yy})
 end
 
+function updateScreen()
+	buildGUI()
+	gfx.update()
+end
 function onKey(key,state)
 	--TODO”
 	if(state == 'up') then
   		if(key == 'Up') then
 	  		--Up
 	  		moveHighlightedInputField(key)
+	  		updateScreen()
 	  	elseif(key == 'Down') then
 	  		--Down
 	  	  	moveHighlightedInputField(key)
+	  	  	updateScreen()
 	  	elseif(key=='Return')then
 	  		addPizzeria()
-	  		print(pizzeriaToAdd)
-
+	  		updateScreen()
 	  	elseif(key == 'blue') then
-	  		addToForm(pizzeriaToAdd)
 	  	  	assert(loadfile("RegistrationStep3.lua"))(newForm)
 
 	  	elseif(key == 'red')then
@@ -203,7 +190,8 @@ end
 --Main method
 function main()
 	checkForm()
-	buildGUI()
+	readPizzeriaFromFile()
+	updateScreen()	
 end
 main()
 

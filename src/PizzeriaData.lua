3e65355b7378465824e4d3da06a2d16bf15997dc
Kalle Olsson <kalleolsson92@gmail.com>
2014-11-27 15:23:18 +0100

    Deleted rar stuff

---
+++ src/PizzeriaData.lua
@@ -0,0 +1,410 @@
+return {
+-- Table: {1}
+{
+   {2},
+   {3},
+   {4},
+   {5},
+   {6},
+   {7},
+   {8},
+   {9},
+   {10},
+   {11},
+},
+-- Table: {2}
+{
+   ["rating"]="5.0",
+   ["zipCode"]="58434",
+   ["imgPath"]="pizza1.png",
+   ["phoneNr"]="010-1111111",
+   ["pizzas"]={12},
+   ["drinks"]={13},
+   ["name"]="Pizzeria Mona Lisa",
+   ["city"]="Linköping",
+},
+-- Table: {3}
+{
+   ["rating"]="5.0",
+   ["zipCode"]="58436",
+   ["imgPath"]="pizza2.png",
+   ["phoneNr"]="010-1111112",
+   ["pizzas"]={12},
+   ["drinks"]={13},
+   ["name"]="Pizzeria Baguetten",
+   ["city"]="Linköping",
+},
+-- Table: {4}
+{
+   ["rating"]="5.0",
+   ["zipCode"]="58220",
+   ["imgPath"]="pizza3.png",
+   ["phoneNr"]="010-1111113",
+   ["pizzas"]={12},
+   ["drinks"]={13},
+   ["name"]="Pizzeria Florens",
+   ["city"]="Linköping",
+},
+-- Table: {5}
+{
+   ["rating"]="5.0",
+   ["zipCode"]="58217",
+   ["imgPath"]="pizza4.png",
+   ["phoneNr"]="010-1111114",
+   ["pizzas"]={12},
+   ["drinks"]={13},
+   ["name"]="Pizzeria Bari",
+   ["city"]="Linköping",
+},
+-- Table: {6}
+{
+   ["rating"]="5.0",
+   ["zipCode"]="58234",
+   ["imgPath"]="pizza5.png",
+   ["phoneNr"]="010-1111115",
+   ["pizzas"]={12},
+   ["drinks"]={13},
+   ["name"]="Pizzeria Victoria",
+   ["city"]="Linköping",
+},
+-- Table: {7}
+{
+   ["rating"]="5.0",
+   ["zipCode"]="58214",
+   ["imgPath"]="pizza6.png",
+   ["phoneNr"]="010-1111116",
+   ["pizzas"]={12},
+   ["drinks"]={13},
+   ["name"]="Pizzeria La Luna",
+   ["city"]="Linköping",
+},
+-- Table: {8}
+{
+   ["rating"]="5.0",
+   ["zipCode"]="58233",
+   ["imgPath"]="pizza7.png",
+   ["phoneNr"]="010-1111117",
+   ["pizzas"]={12},
+   ["drinks"]={13},
+   ["name"]="Pizzafiket",
+   ["city"]="Linköping",
+},
+-- Table: {9}
+{
+   ["rating"]="5.0",
+   ["zipCode"]="58232",
+   ["imgPath"]="pizza8.png",
+   ["phoneNr"]="010-1111118",
+   ["pizzas"]={12},
+   ["drinks"]={13},
+   ["name"]="Mamma Mia",
+   ["city"]="Linköping",
+},
+-- Table: {10}
+{
+   ["rating"]="5.0",
+   ["zipCode"]="58248",
+   ["imgPath"]="pizza9.png",
+   ["phoneNr"]="010-1111119",
+   ["pizzas"]={12},
+   ["drinks"]={13},
+   ["name"]="Pizzeria Montecarlo",
+   ["city"]="Linköping",
+},
+-- Table: {11}
+{
+   ["rating"]="5.0",
+   ["zipCode"]="58726",
+   ["imgPath"]="pizza10.png",
+   ["phoneNr"]="010-1111110",
+   ["pizzas"]={12},
+   ["drinks"]={13},
+   ["name"]="Pizzeria Tropicana",
+   ["city"]="Linköping",
+},
+-- Table: {12}
+{
+   {14},
+   {15},
+   {16},
+   {17},
+   {18},
+   {19},
+   {20},
+   {21},
+   {22},
+   {23},
+   {24},
+   {25},
+   {26},
+   {27},
+   {28},
+   {29},
+   {30},
+   {31},
+   {32},
+   {33},
+},
+-- Table: {13}
+{
+   {34},
+   {35},
+   {36},
+   {37},
+},
+-- Table: {14}
+{
+   ["price"]="70",
+   ["ingredients"]={38},
+   ["name"]="Vesuvio",
+},
+-- Table: {15}
+{
+   ["price"]="75",
+   ["ingredients"]={39},
+   ["name"]="Kebab",
+},
+-- Table: {16}
+{
+   ["price"]="70",
+   ["ingredients"]={40},
+   ["name"]="Mexicana",
+},
+-- Table: {17}
+{
+   ["price"]="65",
+   ["ingredients"]={41},
+   ["name"]="Azteka",
+},
+-- Table: {18}
+{
+   ["price"]="70",
+   ["ingredients"]={42},
+   ["name"]="Kebab Special",
+},
+-- Table: {19}
+{
+   ["price"]="65",
+   ["ingredients"]={43},
+   ["name"]="Kebabtallrik",
+},
+-- Table: {20}
+{
+   ["price"]="70",
+   ["ingredients"]={44},
+   ["name"]="Kebabrulle",
+},
+-- Table: {21}
+{
+   ["price"]="60",
+   ["ingredients"]={45},
+   ["name"]="Hawaii",
+},
+-- Table: {22}
+{
+   ["price"]="85",
+   ["ingredients"]={46},
+   ["name"]="Poker",
+},
+-- Table: {23}
+{
+   ["price"]="75",
+   ["ingredients"]={47},
+   ["name"]="Salami",
+},
+-- Table: {24}
+{
+   ["price"]="60",
+   ["ingredients"]={48},
+   ["name"]="Vegetaria",
+},
+-- Table: {25}
+{
+   ["price"]="60",
+   ["ingredients"]={49},
+   ["name"]="Capricciosa",
+},
+-- Table: {26}
+{
+   ["price"]="80",
+   ["ingredients"]={50},
+   ["name"]="Tono",
+},
+-- Table: {27}
+{
+   ["price"]="60",
+   ["ingredients"]={51},
+   ["name"]="Margaritha",
+},
+-- Table: {28}
+{
+   ["price"]="60",
+   ["ingredients"]={52},
+   ["name"]="Tutti frutti",
+},
+-- Table: {29}
+{
+   ["price"]="65",
+   ["ingredients"]={53},
+   ["name"]="Calzone",
+},
+-- Table: {30}
+{
+   ["price"]="60",
+   ["ingredients"]={54},
+   ["name"]="Honolulu",
+},
+-- Table: {31}
+{
+   ["price"]="60",
+   ["ingredients"]={55},
+   ["name"]="Kycklingpizza",
+},
+-- Table: {32}
+{
+   ["price"]="70",
+   ["ingredients"]={56},
+   ["name"]="Opera",
+},
+-- Table: {33}
+{
+   ["price"]="80",
+   ["ingredients"]={57},
+   ["name"]="Disco",
+},
+-- Table: {34}
+{
+   ["name"]="Coca cola",
+   ["price"]="10",
+},
+-- Table: {35}
+{
+   ["name"]="Fanta",
+   ["price"]="10",
+},
+-- Table: {36}
+{
+   ["name"]="Ramlösa",
+   ["price"]="10",
+},
+-- Table: {37}
+{
+   ["name"]="Julmust",
+   ["price"]="10",
+},
+-- Table: {38}
+{
+   "Skinka",
+   "Ost",
+},
+-- Table: {39}
+{
+   "Skinka",
+   "Ost",
+   "Kebab",
+},
+-- Table: {40}
+{
+   "Skinka",
+   "Ost",
+   "Taco",
+},
+-- Table: {41}
+{
+   "Skinka",
+   "Ost",
+   "Jalapenos",
+},
+-- Table: {42}
+{
+   "Kebab",
+   "Ost",
+   "Pommes",
+},
+-- Table: {43}
+{
+   "Kebab",
+   "Pommes",
+   "Sallad",
+},
+-- Table: {44}
+{
+   "Kebab",
+   "Bröd",
+   "Sås",
+},
+-- Table: {45}
+{
+   "Skinka",
+   "Ost",
+   "Ananas",
+},
+-- Table: {46}
+{
+   "Oxfile",
+   "Ost",
+   "Champinjoner",
+},
+-- Table: {47}
+{
+   "Salami",
+   "Ost",
+},
+-- Table: {48}
+{
+   "Champinjoner",
+   "Ost",
+   "Oliver",
+},
+-- Table: {49}
+{
+   "Skinka",
+   "Ost",
+   "Champinjoner",
+},
+-- Table: {50}
+{
+   "Tonfisk",
+   "Ost",
+},
+-- Table: {51}
+{
+   "Ost",
+},
+-- Table: {52}
+{
+   "Ost",
+   "Ananas",
+   "Banan",
+},
+-- Table: {53}
+{
+   "Inbakad",
+   "Skinka",
+   "Ost",
+},
+-- Table: {54}
+{
+   "Banan",
+   "Ost",
+   "Ananas",
+},
+-- Table: {55}
+{
+   "Kyckling",
+   "Ost",
+   "Curry",
+},
+-- Table: {56}
+{
+   "Salami",
+   "Ost",
+   "Oliver",
+},
+-- Table: {57}
+{
+   "Korv",
+   "Fisk",
+   "BBQ",
+},
+}

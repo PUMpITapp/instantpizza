c6fc0f9ca4c328d533fb1954da1ede2924115d6d
wille <wilbe044@student.liu.se>
2014-11-27 15:11:15 +0100

    Added pizzerias and pizzas

--- src/IOHandler.lua
+++ src/IOHandler.lua
@@ -83,9 +83,7 @@ end
 
 function ioHandler.addTestPizzerias()
   drinks ={}
-  drinks2 = {}
   pizzas = {}
-  pizzas2 = {}
   pizzeriasTable = {}
 
   --Create drinks
@@ -96,35 +94,76 @@ function ioHandler.addTestPizzerias()
 
   --Creates pizzas. {name,price,{ingredients}}
   Pizza1 = Pizza:new("Vesuvio","70",{"Skinka","Ost"})
-  Pizza2 = Pizza:new("Kebab","75",{"Skinka","Ost"})
-  Pizza3 = Pizza:new("Mexicana","70",{"Skinka","Ost"})
-  Pizza4 = Pizza:new("Azteka","65",{"Skinka","Ost"})
-  Pizza5 = Pizza:new("Kebab Special","70",{"Skinka","Ost"})
-  Pizza6 = Pizza:new("Kebabtallrik","65",{"Skinka","Ost"})
-  Pizza7 = Pizza:new("Kebabrulle","70",{"Skinka","Ost"})
-  Pizza8 = Pizza:new("Hawaii","60",{"Skinka","Ost"})
+  Pizza2 = Pizza:new("Kebab","75",{"Skinka","Ost","Kebab"})
+  Pizza3 = Pizza:new("Mexicana","70",{"Skinka","Ost","Taco"})
+  Pizza4 = Pizza:new("Azteka","65",{"Skinka","Ost","Jalapenos"})
+  Pizza5 = Pizza:new("Kebab Special","70",{"Kebab","Ost","Pommes"})
+  Pizza6 = Pizza:new("Kebabtallrik","65",{"Kebab","Pommes","Sallad"})
+  Pizza7 = Pizza:new("Kebabrulle","70",{"Kebab","Bröd","Sås"})
+  Pizza8 = Pizza:new("Hawaii","60",{"Skinka","Ost","Ananas"})
+  Pizza9 = Pizza:new("Poker","85",{"Oxfile","Ost","Champinjoner"})
+  Pizza10 = Pizza:new("Salami","75",{"Salami","Ost"})
+  Pizza11 = Pizza:new("Vegetaria","60",{"Champinjoner","Ost","Oliver"})
+  Pizza12 = Pizza:new("Capricciosa","60",{"Skinka","Ost","Champinjoner"})
+  Pizza13 = Pizza:new("Tono","80",{"Tonfisk","Ost"})
+  Pizza14 = Pizza:new("Margaritha","60",{"Ost"})
+  Pizza15 = Pizza:new("Tutti frutti","60",{"Ost","Ananas","Banan"})
+  Pizza16 = Pizza:new("Calzone","65",{"Inbakad","Skinka","Ost"})
+  Pizza17 = Pizza:new("Honolulu","60",{"Banan","Ost","Ananas"})
+  Pizza18 = Pizza:new("Kycklingpizza","60",{"Kyckling","Ost","Curry"})
+  Pizza19 = Pizza:new("Opera","70",{"Salami","Ost","Oliver"})
+  Pizza20 = Pizza:new("Disco","80",{"Korv","Fisk","BBQ"})
+
   --Put drink1 and 2  into table drinks
   drinks[1] = Drink1
   drinks[2] = Drink2
+  drinks[3] = Drink3
+  drinks[4] = Drink4
 
-  drinks2[1] = Drink3
-  drinks2[2] = Drink4
   --Put pizzas into pizzas table
   pizzas[1] = Pizza1
   pizzas[2] = Pizza2
   pizzas[3] = Pizza3
   pizzas[4] = Pizza4
+  pizzas[5] = Pizza5
+  pizzas[6] = Pizza6
+  pizzas[7] = Pizza7
+  pizzas[8] = Pizza8
+  pizzas[9] = Pizza9
+  pizzas[10] = Pizza10
+  pizzas[11] = Pizza11
+  pizzas[12] = Pizza12
+  pizzas[13] = Pizza13
+  pizzas[14] = Pizza14
+  pizzas[15] = Pizza15
+  pizzas[16] = Pizza16
+  pizzas[17] = Pizza17
+  pizzas[18] = Pizza18
+  pizzas[19] = Pizza19
+  pizzas[20] = Pizza20
 
-  pizzas2[1] = Pizza5
-  pizzas2[2] = Pizza6
-  pizzas2[3] = Pizza7
-  pizzas2[4] = Pizza8
   --Create pizzerias
   Pizzeria1 = Pizzeria:new("Pizzeria Mona Lisa","Linköping","58434","010-1111111","pizza1.png","5.0",pizzas,drinks)
-  Pizzeria2 = Pizzeria:new("Pizzeria Baguetten","Linköping","58436","010-1111112","pizza2.png","5.0",pizzas2,drinks2)
+  Pizzeria2 = Pizzeria:new("Pizzeria Baguetten","Linköping","58436","010-1111112","pizza2.png","5.0",pizzas,drinks)
+  Pizzeria3 = Pizzeria:new("Pizzeria Florens","Linköping","58220","010-1111113","pizza3.png","5.0",pizzas,drinks)
+  Pizzeria4 = Pizzeria:new("Pizzeria Bari","Linköping","58217","010-1111114","pizza4.png","5.0",pizzas,drinks)
+  Pizzeria5 = Pizzeria:new("Pizzeria Victoria","Linköping","58234","010-1111115","pizza5.png","5.0",pizzas,drinks)
+  Pizzeria6 = Pizzeria:new("Pizzeria La Luna","Linköping","58214","010-1111116","pizza6.png","5.0",pizzas,drinks)
+  Pizzeria7 = Pizzeria:new("Pizzafiket","Linköping","58233","010-1111117","pizza7.png","5.0",pizzas,drinks)
+  Pizzeria8 = Pizzeria:new("Mamma Mia","Linköping","58232","010-1111118","pizza8.png","5.0",pizzas,drinks)
+  Pizzeria9 = Pizzeria:new("Pizzeria Montecarlo","Linköping","58248","010-1111119","pizza9.png","5.0",pizzas,drinks)
+  Pizzeria10 = Pizzeria:new("Pizzeria Tropicana","Linköping","58726","010-1111110","pizza10.png","5.0",pizzas,drinks)
   --Put pizzerias in table
   pizzeriasTable[1] = Pizzeria1
   pizzeriasTable[2] = Pizzeria2
+  pizzeriasTable[3] = Pizzeria3
+  pizzeriasTable[4] = Pizzeria4
+  pizzeriasTable[5] = Pizzeria5
+  pizzeriasTable[6] = Pizzeria6
+  pizzeriasTable[7] = Pizzeria7
+  pizzeriasTable[8] = Pizzeria8
+  pizzeriasTable[9] = Pizzeria9
+  pizzeriasTable[10] = Pizzeria10
   table.save(pizzeriasTable,dir .. "PizzeriaData.lua")
 end
 
--- src/PizzeriaData.lua
+++ src/PizzeriaData.lua
@@ -3,159 +3,408 @@ return {
 {
    {2},
    {3},
-},
--- Table: {2}
-{
-   ["pizzas"]={4},
-   ["drinks"]={5},
-   ["zipCode"]="58434",
-   ["rating"]="5.0",
-   ["phoneNr"]="010-1111111",
-   ["city"]="Linköping",
-   ["name"]="Pizzeria Mona Lisa",
-   ["imgPath"]="pizza1.png",
-},
--- Table: {3}
-{
-   ["pizzas"]={6},
-   ["drinks"]={7},
-   ["zipCode"]="58436",
-   ["rating"]="5.0",
-   ["phoneNr"]="010-1111112",
-   ["city"]="Linköping",
-   ["name"]="Pizzeria Baguetten",
-   ["imgPath"]="pizza2.png",
-},
--- Table: {4}
-{
+   {4},
+   {5},
+   {6},
+   {7},
    {8},
    {9},
    {10},
    {11},
 },
+-- Table: {2}
+{
+   ["phoneNr"]="010-1111111",
+   ["imgPath"]="pizza1.png",
+   ["drinks"]={12},
+   ["rating"]="5.0",
+   ["name"]="Pizzeria Mona Lisa",
+   ["pizzas"]={13},
+   ["zipCode"]="58434",
+   ["city"]="Linköping",
+},
+-- Table: {3}
+{
+   ["phoneNr"]="010-1111112",
+   ["imgPath"]="pizza2.png",
+   ["drinks"]={12},
+   ["rating"]="5.0",
+   ["name"]="Pizzeria Baguetten",
+   ["pizzas"]={13},
+   ["zipCode"]="58436",
+   ["city"]="Linköping",
+},
+-- Table: {4}
+{
+   ["phoneNr"]="010-1111113",
+   ["imgPath"]="pizza3.png",
+   ["drinks"]={12},
+   ["rating"]="5.0",
+   ["name"]="Pizzeria Florens",
+   ["pizzas"]={13},
+   ["zipCode"]="58220",
+   ["city"]="Linköping",
+},
 -- Table: {5}
 {
-   {12},
-   {13},
+   ["phoneNr"]="010-1111114",
+   ["imgPath"]="pizza4.png",
+   ["drinks"]={12},
+   ["rating"]="5.0",
+   ["name"]="Pizzeria Bari",
+   ["pizzas"]={13},
+   ["zipCode"]="58217",
+   ["city"]="Linköping",
 },
 -- Table: {6}
+{
+   ["phoneNr"]="010-1111115",
+   ["imgPath"]="pizza5.png",
+   ["drinks"]={12},
+   ["rating"]="5.0",
+   ["name"]="Pizzeria Victoria",
+   ["pizzas"]={13},
+   ["zipCode"]="58234",
+   ["city"]="Linköping",
+},
+-- Table: {7}
+{
+   ["phoneNr"]="010-1111116",
+   ["imgPath"]="pizza6.png",
+   ["drinks"]={12},
+   ["rating"]="5.0",
+   ["name"]="Pizzeria La Luna",
+   ["pizzas"]={13},
+   ["zipCode"]="58214",
+   ["city"]="Linköping",
+},
+-- Table: {8}
+{
+   ["phoneNr"]="010-1111117",
+   ["imgPath"]="pizza7.png",
+   ["drinks"]={12},
+   ["rating"]="5.0",
+   ["name"]="Pizzafiket",
+   ["pizzas"]={13},
+   ["zipCode"]="58233",
+   ["city"]="Linköping",
+},
+-- Table: {9}
+{
+   ["phoneNr"]="010-1111118",
+   ["imgPath"]="pizza8.png",
+   ["drinks"]={12},
+   ["rating"]="5.0",
+   ["name"]="Mamma Mia",
+   ["pizzas"]={13},
+   ["zipCode"]="58232",
+   ["city"]="Linköping",
+},
+-- Table: {10}
+{
+   ["phoneNr"]="010-1111119",
+   ["imgPath"]="pizza9.png",
+   ["drinks"]={12},
+   ["rating"]="5.0",
+   ["name"]="Pizzeria Montecarlo",
+   ["pizzas"]={13},
+   ["zipCode"]="58248",
+   ["city"]="Linköping",
+},
+-- Table: {11}
+{
+   ["phoneNr"]="010-1111110",
+   ["imgPath"]="pizza10.png",
+   ["drinks"]={12},
+   ["rating"]="5.0",
+   ["name"]="Pizzeria Tropicana",
+   ["pizzas"]={13},
+   ["zipCode"]="58726",
+   ["city"]="Linköping",
+},
+-- Table: {12}
 {
    {14},
    {15},
    {16},
    {17},
 },
--- Table: {7}
+-- Table: {13}
 {
    {18},
    {19},
+   {20},
+   {21},
+   {22},
+   {23},
+   {24},
+   {25},
+   {26},
+   {27},
+   {28},
+   {29},
+   {30},
+   {31},
+   {32},
+   {33},
+   {34},
+   {35},
+   {36},
+   {37},
 },
--- Table: {8}
-{
-   ["price"]="70",
-   ["name"]="Vesuvio",
-   ["ingredients"]={20},
-},
--- Table: {9}
-{
-   ["price"]="75",
-   ["name"]="Kebab",
-   ["ingredients"]={21},
-},
--- Table: {10}
-{
-   ["price"]="70",
-   ["name"]="Mexicana",
-   ["ingredients"]={22},
-},
--- Table: {11}
-{
-   ["price"]="65",
-   ["name"]="Azteka",
-   ["ingredients"]={23},
-},
--- Table: {12}
+-- Table: {14}
 {
    ["name"]="Coca cola",
    ["price"]="10",
 },
--- Table: {13}
+-- Table: {15}
 {
    ["name"]="Fanta",
    ["price"]="10",
 },
--- Table: {14}
-{
-   ["price"]="70",
-   ["name"]="Kebab Special",
-   ["ingredients"]={24},
-},
--- Table: {15}
-{
-   ["price"]="65",
-   ["name"]="Kebabtallrik",
-   ["ingredients"]={25},
-},
 -- Table: {16}
-{
-   ["price"]="70",
-   ["name"]="Kebabrulle",
-   ["ingredients"]={26},
-},
--- Table: {17}
-{
-   ["price"]="60",
-   ["name"]="Hawaii",
-   ["ingredients"]={27},
-},
--- Table: {18}
 {
    ["name"]="Ramlösa",
    ["price"]="10",
 },
--- Table: {19}
+-- Table: {17}
 {
    ["name"]="Julmust",
    ["price"]="10",
 },
+-- Table: {18}
+{
+   ["name"]="Vesuvio",
+   ["ingredients"]={38},
+   ["price"]="70",
+},
+-- Table: {19}
+{
+   ["name"]="Kebab",
+   ["ingredients"]={39},
+   ["price"]="75",
+},
 -- Table: {20}
 {
-   "Skinka",
-   "Ost",
+   ["name"]="Mexicana",
+   ["ingredients"]={40},
+   ["price"]="70",
 },
 -- Table: {21}
 {
-   "Skinka",
-   "Ost",
+   ["name"]="Azteka",
+   ["ingredients"]={41},
+   ["price"]="65",
 },
 -- Table: {22}
 {
-   "Skinka",
-   "Ost",
+   ["name"]="Kebab Special",
+   ["ingredients"]={42},
+   ["price"]="70",
 },
 -- Table: {23}
 {
-   "Skinka",
-   "Ost",
+   ["name"]="Kebabtallrik",
+   ["ingredients"]={43},
+   ["price"]="65",
 },
 -- Table: {24}
 {
-   "Skinka",
-   "Ost",
+   ["name"]="Kebabrulle",
+   ["ingredients"]={44},
+   ["price"]="70",
 },
 -- Table: {25}
 {
-   "Skinka",
-   "Ost",
+   ["name"]="Hawaii",
+   ["ingredients"]={45},
+   ["price"]="60",
 },
 -- Table: {26}
 {
-   "Skinka",
-   "Ost",
+   ["name"]="Poker",
+   ["ingredients"]={46},
+   ["price"]="85",
 },
 -- Table: {27}
 {
+   ["name"]="Salami",
+   ["ingredients"]={47},
+   ["price"]="75",
+},
+-- Table: {28}
+{
+   ["name"]="Vegetaria",
+   ["ingredients"]={48},
+   ["price"]="60",
+},
+-- Table: {29}
+{
+   ["name"]="Capricciosa",
+   ["ingredients"]={49},
+   ["price"]="60",
+},
+-- Table: {30}
+{
+   ["name"]="Tono",
+   ["ingredients"]={50},
+   ["price"]="80",
+},
+-- Table: {31}
+{
+   ["name"]="Margaritha",
+   ["ingredients"]={51},
+   ["price"]="60",
+},
+-- Table: {32}
+{
+   ["name"]="Tutti frutti",
+   ["ingredients"]={52},
+   ["price"]="60",
+},
+-- Table: {33}
+{
+   ["name"]="Calzone",
+   ["ingredients"]={53},
+   ["price"]="65",
+},
+-- Table: {34}
+{
+   ["name"]="Honolulu",
+   ["ingredients"]={54},
+   ["price"]="60",
+},
+-- Table: {35}
+{
+   ["name"]="Kycklingpizza",
+   ["ingredients"]={55},
+   ["price"]="60",
+},
+-- Table: {36}
+{
+   ["name"]="Opera",
+   ["ingredients"]={56},
+   ["price"]="70",
+},
+-- Table: {37}
+{
+   ["name"]="Disco",
+   ["ingredients"]={57},
+   ["price"]="80",
+},
+-- Table: {38}
+{
    "Skinka",
    "Ost",
+},
+-- Table: {39}
+{
+   "Skinka",
+   "Ost",
+   "Kebab",
+},
+-- Table: {40}
+{
+   "Skinka",
+   "Ost",
+   "Taco",
+},
+-- Table: {41}
+{
+   "Skinka",
+   "Ost",
+   "Jalapenos",
+},
+-- Table: {42}
+{
+   "Kebab",
+   "Ost",
+   "Pommes",
+},
+-- Table: {43}
+{
+   "Kebab",
+   "Pommes",
+   "Sallad",
+},
+-- Table: {44}
+{
+   "Kebab",
+   "Bröd",
+   "Sås",
+},
+-- Table: {45}
+{
+   "Skinka",
+   "Ost",
+   "Ananas",
+},
+-- Table: {46}
+{
+   "Oxfile",
+   "Ost",
+   "Champinjoner",
+},
+-- Table: {47}
+{
+   "Salami",
+   "Ost",
+},
+-- Table: {48}
+{
+   "Champinjoner",
+   "Ost",
+   "Oliver",
+},
+-- Table: {49}
+{
+   "Skinka",
+   "Ost",
+   "Champinjoner",
+},
+-- Table: {50}
+{
+   "Tonfisk",
+   "Ost",
+},
+-- Table: {51}
+{
+   "Ost",
+},
+-- Table: {52}
+{
+   "Ost",
+   "Ananas",
+   "Banan",
+},
+-- Table: {53}
+{
+   "Inbakad",
+   "Skinka",
+   "Ost",
+},
+-- Table: {54}
+{
+   "Banan",
+   "Ost",
+   "Ananas",
+},
+-- Table: {55}
+{
+   "Kyckling",
+   "Ost",
+   "Curry",
+},
+-- Table: {56}
+{
+   "Salami",
+   "Ost",
+   "Oliver",
+},
+-- Table: {57}
+{
+   "Korv",
+   "Fisk",
+   "BBQ",
 },
 }
